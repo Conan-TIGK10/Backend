8eb04a0840c12ed51bc54dd3c6b111004c051a5b
Måns Svensson <mans.cool@hotmail.com>
2020-04-01 14:43:35 +0200

    added routers and basic setup

---
+++ conan-backend/src/pl-express/loaders/express/routers/position.ts
@@ -0,0 +1,18 @@
+/*
+ * This file contains the routing of the position of the mower
+ */
+
+import express from 'express'
+
+const router = express.Router()
+
+// NEED STUFF FROM BUSINESSLAYER
+
+router.get('/', (request,response) => {
+ //TODO
+});
+
+router.post('/', (request,response) => {
+    //TODO
+});
+
---
+++ conan-backend/src/pl-express/loaders/express/routes.ts
@@ -0,0 +1,11 @@
+/*
+ * This file contains all the routes 
+ * and is the main entrypoint to all 
+ * parts of the API.
+ */
+
+import express from 'express'
+
+ const router = express.Router();
+
+ 
