2f81f8ef86861783f2406a82a42f2ababf5de584
maxbekkhus <maxx_b@hotmail.com>
2020-04-29 15:32:53 +0200

    Added a express web app container with webpack and node

---
+++ conan-web/src/express/index.ts
@@ -0,0 +1,39 @@
+/* NPM PACKAGES */
+import express from "express"
+
+/*
+    run(): Promise<void>
+    ---------------------------------------------
+    Constructs Express application, loads functionality
+    and starts listening for connections.
+*/
+const run = async () => {
+  const app: express.Application = express()
+
+  app.use(express.static('./dist/public'))
+
+  const appName: string | undefined = process.env.APP_NAME
+  app.get('/', (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+            <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>${appName}</title>
+                <script src="/bundle.js" defer></script>
+            </head>
+            <body>
+                <div id="root"></div>
+            </body>
+        </html>
+    `)
+  })
+
+  const port: string | undefined = process.env.PORT_WEB
+
+  /* BINDS AND LISTENS TO CONNECTIONS */
+  app.listen(port, () => {
+    console.log(`conan-web is listening on port ${port}`)
+  })
+}
+
+run()
---
+++ conan-web/webpack.config.js
@@ -0,0 +1,58 @@
+/* NPM PACKAGES */
+const path = require('path')
+const NodemonPlugin = require('nodemon-webpack-plugin')
+
+/* ENTRY */
+const entry = {
+    main: './src/express/index.ts'
+}
+
+/* TARGET */
+const target = 'node'
+
+/* MODE */
+const mode = process.env.NODE_ENV
+
+/* DEVTOOL */
+const devtool = 'inline-source-map'
+
+/* MODULE */
+const modules = {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }
+    ]
+}
+
+/* RESOLVE */
+const resolve = {
+    extensions: [ '.tsx', '.ts', '.js' ]
+}
+
+/* PLUGINS */
+const plugins = [
+    new NodemonPlugin({
+        verbose: false
+    })
+]
+
+/* OUTPUT */
+const output = {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+}
+
+/* EXPORT CONFIG */
+module.exports = {
+    entry: entry,
+    target: target,
+    mode: mode,
+    devtool: devtool,
+    module: modules,
+    resolve: resolve,
+    plugins: plugins,
+    output: output
+  };
