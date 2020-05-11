5555fc7a5fcf954c2f63ee6a95e4aa9d21d8d0fc
maxbekkhus <maxx_b@hotmail.com>
2020-05-12 01:34:21 +0200

    Removed build from gitignore in conan-react

---
+++ conan-react/build/precache-manifest.8cff2895a7803ec5faa42dd86bcc679c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1c32c437a3f3076e8f3610f8cd4f4b",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "3c8443c92a7066ec3968",
+    "url": "/web/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ba76085dd9be421fe804",
+    "url": "/web/static/css/main.22be7de8.chunk.css"
+  },
+  {
+    "revision": "3c8443c92a7066ec3968",
+    "url": "/web/static/js/2.0e9ea11d.chunk.js"
+  },
+  {
+    "revision": "5477adf395533eaa5bd24fda08f1c2f1",
+    "url": "/web/static/js/2.0e9ea11d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba76085dd9be421fe804",
+    "url": "/web/static/js/main.c5c83126.chunk.js"
+  },
+  {
+    "revision": "484b30c364eb778eec29",
+    "url": "/web/static/js/runtime-main.de2d6245.js"
+  }
+]);
